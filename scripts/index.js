b08a6dfde986800244af4e9d5d42a608706afc41
Matvey Kyshtymov <matvey.kyshtymov@gmail.com>
2025-03-24 18:40:23 +0300

    enhance code readability

--- scripts/index.js
+++ scripts/index.js
@@ -7,15 +7,17 @@ const cardTemplate = document.querySelector('#card-template').content;
 const cardAdd = (card, cardDelete) => {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = card.name;
   cardElement.querySelector('.card__title').textContent = card.name;
   cardElement.querySelector('.card__delete-button').addEventListener('click', () => cardDelete(cardElement));
-  cardList.append(cardElement);
+  return cardElement;
 }
 // @todo: Функция удаления карточки
-const cardDelete = del => {
-  del.remove();
+const cardDelete = cardElement => {
+  cardElement.remove();
 }
 // @todo: Вывести карточки на страницу
 initialCards.forEach(card => {
-  cardAdd(card, cardDelete);
+  const cardElement = cardAdd(card, cardDelete);
+  cardList.append(cardElement);
 });
